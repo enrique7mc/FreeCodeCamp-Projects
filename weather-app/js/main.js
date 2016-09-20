9f7e71064c0cbcd617c562919b55dcd3cec71fe0
enrique.munguia <enrique.munguia@hulu.com>
2016-09-20 12:02:03 -0500

    add loading gif

--- weather-app/js/main.js
+++ weather-app/js/main.js
@@ -6,12 +6,16 @@
   var CONDITIONS_SELECTOR = '.conditions';
   var WIND_SELECTOR = '.wind';
   var IMG_SELECTOR = '.weather-icon';
+  var SECTION_SELECTOR = '.weather';
+  var LOADING_SELECTOR = '.load-icon';
 
   var temperatureElement = document.querySelector(TEMPERATURE_SELECTOR);
   var locationElement = document.querySelector(LOCATION_SELECTOR);
   var conditionsElement = document.querySelector(CONDITIONS_SELECTOR);
   var windElement = document.querySelector(WIND_SELECTOR);
   var imageElement = document.querySelector(IMG_SELECTOR);
+  var sectionElement = document.querySelector(SECTION_SELECTOR);
+  var loadingElement = document.querySelector(LOADING_SELECTOR);
 
   var API_ENDPOINT = 'http://api.openweathermap.org/data/2.5/weather';
   var API_KEY = 'c2389f8342748200b6a1cb23ed83fe1f';
@@ -42,10 +46,12 @@
     conditionsElement.textContent = conditions;
     windElement.textContent = 'Wind: ' + windSpeed + ' m/s';
     imageElement.src = 'img/weather-icons/' + icon + '.png';
+
+    sectionElement.classList.remove('hide');
+    loadingElement.classList.add('hide');
   }
 
   function getWeather(latitude, longitude, city) {
-    console.log(latitude, longitude, city);
     var URL = API_ENDPOINT
       + '?lat=' + latitude
       + '&lon=' + longitude
@@ -70,7 +76,6 @@
     //   "wind":{"speed":0.76,"deg":26.0017},
     //   "rain":{"3h":0.255},
     //   "clouds":{"all":32},
-    //
     //   "dt":1474321837,
     //   "sys":{"message":0.1699,"country":"MX","sunrise":1474287899,"sunset":1474331638},
     //   "id":3827407,
@@ -78,7 +83,9 @@
     //   "cod":200
     // };
     // response.city = city;
-    // showWeather(response);
+    // setTimeout(function () {
+    //   showWeather(response);
+    // }, 2000);
   }
 
   function getLocation(callback) {
