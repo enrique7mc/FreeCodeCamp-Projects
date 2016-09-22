2919394d6f74cf889034a54d2b22f9a4d8af78d3
enrique.munguia <enrique.munguia@hulu.com>
2016-09-22 18:51:45 -0500

    add html preview

--- live-code/js/app.js
+++ live-code/js/app.js
@@ -7,12 +7,13 @@ import $ from 'jquery';
 const $htmlTextArea = $('[name=html-text-area]');
 const $cssTextArea = $('[name=css-text-area]');
 const $jsTextArea = $('[name=javascript-text-area]');
+const $output = $('.output');
 
 const showCode = ($element, filePath) => {
   $.get(filePath)
   .done((data) => {
-    // console.log(data);
-    $element.text(`${data}`);
+    $element.val(`${data}`);
+    $output.append(data);
   })
   .fail(() => {
     console.log('error');
@@ -20,5 +21,5 @@ const showCode = ($element, filePath) => {
 };
 
 showCode($htmlTextArea, '../demo-code/demo.html');
-showCode($cssTextArea, '../demo-code/demo.css');
-showCode($jsTextArea, '../demo-code/demo.js');
+// showCode($cssTextArea, '../demo-code/demo.css');
+// showCode($jsTextArea, '../demo-code/demo.js');
